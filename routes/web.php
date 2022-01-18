0f5804830cb471c459ddd584933a1965e44b9e7c
Poos Daniel <poosdani@gmail.com>
2022-01-18 17:13:55 +0100

    /

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ HomeController::class, 'index' ])->name('home');
 
 //Route::get('/statues', [ StatueController::class, 'index' ]);
-Route::resource('statues', StatueController::class);
+Route::resource('/statues', StatueController::class);
